f5671fefc4463b7a5b39bdc88d75677bfcf9e283
Victor Ferraz <58578468+victorHSFerraz@users.noreply.github.com>
2021-01-18 15:44:38 -0300

    Update & Formatação

---
+++ agenda_app/lib/providers/database_provider.dart
@@ -0,0 +1,114 @@
+// nome dos campos na tabela do banco de dados
+import 'dart:async';
+
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+final String contactTable = "contactTable";
+final String idColumn = "idColumn";
+final String nameColumn = "nameColumn";
+final String emailColumn = "emailColumn";
+final String phoneColumn = "phoneColumn";
+
+class DatabaseProvider {
+  static final DatabaseProvider _instance = DatabaseProvider.internal();
+  factory DatabaseProvider() => _instance;
+
+  DatabaseProvider.internal();
+  Database _db;
+
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db;
+    } else {
+      _db = await initDb();
+      return _db;
+    }
+  }
+
+  Future<Database> initDb() async {
+    final databasePath = await getDatabasesPath();
+    final path = join(databasePath, "contactsnew.db");
+
+    return await openDatabase(path, version: 1,
+        onCreate: (Database db, int newerVersion) async {
+      await db.execute(
+          "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT, $phoneColumn TEXT)");
+    });
+  }
+
+  Future<Contact> saveContact(Contact contact) async {
+    Database dbContact = await db;
+    contact.id = await dbContact.insert(contactTable, contact.toMap());
+    return contact;
+  }
+
+  Future<Contact> getContact(int id) async {
+    Database dbContact = await db;
+    List<Map> maps = await dbContact.query(contactTable,
+        columns: [idColumn, nameColumn, emailColumn, phoneColumn],
+        where: "$idColumn = ?",
+        whereArgs: [id]);
+
+    if (maps.length > 0) {
+      return Contact.fromMap(maps.first);
+    } else {
+      return null;
+    }
+  }
+
+  Future<int> deleteContact(int id) async {
+    Database dbContact = await db;
+    return await dbContact
+        .delete(contactTable, where: "$idColumn = ?", whereArgs: [id]);
+  }
+
+  Future<int> updateContact(Contact contact) async {
+    Database dbContact = await db;
+    return await dbContact.update(contactTable, contact.toMap(),
+        where: "$idColumn = ?", whereArgs: [contact.id]);
+  }
+
+  Future<List> getAllContacts() async {
+    Database dbContact = await db;
+    List listMap = await dbContact.rawQuery("SELECT * FROM $contactTable");
+    List<Contact> listContact = List();
+    for (Map m in listMap) {
+      listContact.add(Contact.fromMap(m));
+    }
+    return listContact;
+  }
+}
+
+class Contact {
+  int id;
+  String name;
+  String email;
+  String phone;
+
+  Contact();
+
+  //Construtor que convert os dados de mapa (JSON) para objeto de contato
+  Contact.fromMap(Map map) {
+    id = map[idColumn];
+    name = map[nameColumn];
+    email = map[emailColumn];
+    phone = map[phoneColumn];
+  }
+
+  //Método que transforma o objeto do contato em Mapa (JSON) para armazenar no banco de dados
+  Map toMap() {
+    Map<String, dynamic> map = {
+      nameColumn: name,
+      emailColumn: email,
+      phoneColumn: phone,
+    };
+    // O id pode ser nulo caso o registro esteja sendo criado já
+    // que é o banco de dados que atribui o id ao registro na hora de salvar
+    // Por isso deve testar antes de atribuir
+    if (id != null) {
+      map[idColumn] = id;
+    }
+    return map;
+  }
+}
