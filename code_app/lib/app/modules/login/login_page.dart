9b3cd68798532dbed6fcaf41d8506e7b0845455a
Victor Ferraz <58578468+victorHSFerraz@users.noreply.github.com>
2021-02-18 15:26:00 -0300

    Update

--- code_app/lib/app/modules/login/login_page.dart
+++ code_app/lib/app/modules/login/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:code_app/app/shared/util/FadeAnimation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'login_controller.dart';
@@ -16,6 +17,7 @@ class _LoginPageState extends ModularState<LoginPage, LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Container(
         padding: EdgeInsets.only(top: 20.0),
         width: double.infinity,
@@ -41,16 +43,22 @@ class _LoginPageState extends ModularState<LoginPage, LoginController> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    "Login",
-                    style: TextStyle(color: Colors.white, fontSize: 30),
+                  FadeAnimation(
+                    1.2,
+                    Text(
+                      "Login",
+                      style: TextStyle(color: Colors.white, fontSize: 30),
+                    ),
                   ),
                   SizedBox(
                     height: 5,
                   ),
-                  Text(
-                    "Welcome Back",
-                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  FadeAnimation(
+                    1.3,
+                    Text(
+                      "Bem-vindo!",
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
                   ),
                 ],
               ),
@@ -67,68 +75,129 @@ class _LoginPageState extends ModularState<LoginPage, LoginController> {
                 child: Column(
                   children: [
                     SizedBox(
-                      height: 30,
+                      height: 50,
                     ),
                     Padding(
                       padding: const EdgeInsets.all(20.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                          boxShadow: [
-                            BoxShadow(
-                                color: Color.fromRGBO(102, 0, 204, 3),
-                                blurRadius: 30,
-                                offset: Offset(0, 10))
-                          ],
-                        ),
-                        child: Column(
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                border: Border(
-                                  bottom: BorderSide(color: Colors.grey[200]),
+                      child: FadeAnimation(
+                        1.4,
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Color.fromRGBO(102, 0, 204, 3),
+                                  blurRadius: 5,
+                                  offset: Offset(0, 1))
+                            ],
+                          ),
+                          child: Column(
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  border: Border(
+                                    bottom: BorderSide(color: Colors.grey[200]),
+                                  ),
+                                ),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                      contentPadding: EdgeInsets.all(20),
+                                      hintText: "Email or Phone Number",
+                                      hintStyle: TextStyle(color: Colors.grey),
+                                      border: InputBorder.none),
                                 ),
                               ),
-                              child: TextField(
-                                decoration: InputDecoration(
-                                    contentPadding: EdgeInsets.all(20),
-                                    hintText: "Email or Phone Number",
-                                    hintStyle: TextStyle(color: Colors.grey),
-                                    border: InputBorder.none),
-                              ),
-                            ),
-                            Container(
-                              child: TextField(
-                                decoration: InputDecoration(
-                                    contentPadding: EdgeInsets.all(20),
-                                    hintText: "Password",
-                                    hintStyle: TextStyle(color: Colors.grey),
-                                    border: InputBorder.none),
-                              ),
-                            )
-                          ],
+                              Container(
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                      contentPadding: EdgeInsets.all(20),
+                                      hintText: "Password",
+                                      hintStyle: TextStyle(color: Colors.grey),
+                                      border: InputBorder.none),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    FadeAnimation(
+                      1.5,
+                      Text(
+                        "Forgot Password?",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    FadeAnimation(
+                      1.6,
+                      Container(
+                        height: 50,
+                        margin: EdgeInsets.symmetric(horizontal: 50),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: Colors.purple[800]),
+                        child: Center(
+                          child: Text(
+                            "Login",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
                         ),
                       ),
                     ),
                     SizedBox(height: 40),
-                    Text(
-                      "Forgot Password?",
-                      style: TextStyle(color: Colors.grey),
+                    FadeAnimation(
+                      1.7,
+                      Text(
+                        "Continue with social media",
+                        style: TextStyle(color: Colors.grey),
+                      ),
                     ),
                     SizedBox(height: 40),
-                    Container(
-                      height: 50,
-                      margin: EdgeInsets.symmetric(horizontal: 50),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(50),
-                          color: Colors.purple[800]),
-                      child: Center(
-                        child: Text(
-                          "Login",
-                          style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold),
-                        ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: FadeAnimation(
+                              1.8,
+                              Container(
+                                height: 50,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50),
+                                    color: Colors.blue),
+                                child: Center(
+                                  child: Text(
+                                    "Facebook",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 30),
+                          Expanded(
+                            child: FadeAnimation(
+                              1.9,
+                              Container(
+                                height: 50,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50),
+                                    color: Colors.black),
+                                child: Center(
+                                  child: Text(
+                                    "GitHub",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     )
                   ],
